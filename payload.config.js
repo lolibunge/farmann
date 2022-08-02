2ce4c5c5bed9b858d7b5bbe6ac1b4aca02de08ab
lolibunge <loli@avanfeel.com>
2022-08-02 10:11:43 -0300

    some commit

--- payload.config.js
+++ payload.config.js
@@ -1,4 +1,5 @@
 import { buildConfig } from 'payload/config';
+import path from 'path';
 
 import Categories from './collections/Categories';
 import Users from './collections/Users';
@@ -8,8 +9,10 @@ import Paddocks from './collections/Paddocks';
 
 export default buildConfig({
   serverURL: 'http://localhost:3000',
+  cookiePrefix: 'payload',
   admin: {
     user: Users.slug,
+    css: path.resolve(__dirname, './assets/styles/stylesheet.scss'),
   },
   collections: [
     Categories,
@@ -17,5 +20,48 @@ export default buildConfig({
     Paddocks,
     Breeds,
     Users,
+    {
+      slug: 'pages',
+      fields: [
+          {
+              name: 'title',
+              type: 'text',
+              required: true,
+          },
+          {
+            name: 'Lista de Animales',
+            type: 'relationship',
+            relationTo: 'animals',
+        },
+          {
+              name: 'content',
+              type: 'richText',
+              required: true,
+          }
+      ]
+  }
   ],
+  upload: {
+    limits: {
+      fileSize: 5000000, // 5MB, written in bytes
+    }
+  },
+  globals: [
+    {
+        slug: 'header',
+        fields: [
+            {
+                name: 'nav',
+                type: 'array',
+                fields: [
+                    {
+                        name: 'page',
+                        type: 'relationship',
+                        relationTo: 'pages',
+                    },
+                ]
+            }
+        ]
+    }
+]
 });
